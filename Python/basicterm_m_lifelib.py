d0f7abc938edab32ff30b7a6ba6391027a719d7e
serenity4 <cedric.bel@hotmail.fr>
2023-08-27 15:13:35 +0100

    Remove unused Python directory

--- Python/basicterm_m_lifelib.py
+++
@@ -1,8 +0,0 @@
-import modelx as mx
-import numpy as np
-
-m = mx.read_model("BasicTerm_M")
-
-def basicterm_m_lifelib():
-    m.Projection.clear_cache = 1
-    return float(np.sum(m.Projection.pv_net_cf()))
